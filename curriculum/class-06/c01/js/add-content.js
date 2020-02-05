186bbc5e8cc1c9380e9f8fbea97da69c089c0348
racarter1215 <racarter1215@gmail.com>
2020-02-05 14:35:23 -0800

    fixed JS error

--- curriculum/class-06/c01/js/add-content.js
+++ curriculum/class-06/c01/js/add-content.js
@@ -5,6 +5,8 @@ var greeting;
 if (hourNow > 18) {
     greeting = 'Good evening!';
 } else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
     greeting = 'Good morning!';
 } else {
     greeting = 'Welcome!';
